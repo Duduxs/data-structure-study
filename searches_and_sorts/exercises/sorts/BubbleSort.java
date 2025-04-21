e864dde338f16cbf6353d9a900edde51526487b6
Eduardo J <duduxss3@gmail.com>
2025-04-21 02:58:37 -0300

    :sparkles: (main) Adiciona complexidade do algoritmo bubbleSort

--- searches_and_sorts/exercises/sorts/BubbleSort.java
+++ searches_and_sorts/exercises/sorts/BubbleSort.java
@@ -11,6 +11,19 @@ public class BubbleSort {
         System.out.println(Arrays.stream(sort(new int[]{38, 23, 8, -5, 16, 29, 0, 13})).mapToObj(x -> String.valueOf(x)).collect(joining(",")));
     }
 
+    /**
+     * Análise da complexidade de tempo
+     * Ω(N) ⇾ Os elementos já estão ordenados.
+     * Θ(N²) ⇾ Necessário ordenar parte dos elementos.
+     * O(N²) ⇾ Os elementos estão na ordem inversa.
+     *
+     * Análise da complexidade de espaço
+     * O(1) ⇾ A memória não aumenta conforme o tamanho da entrada.
+     * Variáveis i, j, hasSwapped e aux só tem seus valores trocados por outros na stack durante o runtime,
+     * mas sem adicionar memória.
+     *
+     * Não citei theta nem ômega, pois nesse caso eles tem o mesmo consumo de memória que o Big O.
+     */
     public static int[] sort(int[] nums) {
 
         for (var i = 0; i < nums.length; i++) {
