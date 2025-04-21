8f8ea0aba1560de7a355f4bd6054678ee7455bf9
Eduardo J <duduxss3@gmail.com>
2025-04-21 02:45:14 -0300

    :sparkles: (main) Melhora algoritmo bubble sort

--- searches_and_sorts/exercises/sorts/BubbleSort.java
+++ searches_and_sorts/exercises/sorts/BubbleSort.java
@@ -8,17 +8,27 @@ public class BubbleSort {
     void main() {
         System.out.println(Arrays.stream(sort(new int[]{20, 9, 86, -2, 16})).mapToObj(x -> String.valueOf(x)).collect(joining(",")));
         System.out.println(Arrays.stream(sort(new int[]{5, 4, 3, 2, 1})).mapToObj(x -> String.valueOf(x)).collect(joining(",")));
+        System.out.println(Arrays.stream(sort(new int[]{38, 23, 8, -5, 16, 29, 0, 13})).mapToObj(x -> String.valueOf(x)).collect(joining(",")));
     }
 
     public static int[] sort(int[] nums) {
 
         for (var i = 0; i < nums.length; i++) {
-            for (var j = 0; j < nums.length - 1; j++) {
+
+            var hasSwapped = false; /** Evita iterações desnecessárias caso tds os elementos em
+             algum momento já estejam ordenados **/
+
+            for (var j = 0; j < nums.length - i - 1; j++) {
                 if (nums[j] > nums[j + 1]) {
                     var aux = nums[j + 1];
                     nums[j + 1] = nums[j];
                     nums[j] = aux;
+                    hasSwapped = true;
                 }
+            }
+
+            if (!hasSwapped) {
+                break;
             }
         }
 
