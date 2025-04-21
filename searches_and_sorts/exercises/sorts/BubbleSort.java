982ab92171314e55a5f5644f8709537a85fdd2da
Eduardo J <duduxss3@gmail.com>
2025-04-21 02:27:24 -0300

    :sparkles: (main) Adiciona algoritmo de ordenação bubble sort

--- searches_and_sorts/exercises/sorts/BubbleSort.java
+++ searches_and_sorts/exercises/sorts/BubbleSort.java
@@ -1,11 +1,27 @@
 package src.searches_and_sorts.exercises.sorts;
 
+import java.util.Arrays;
+import static java.util.stream.Collectors.joining;
+
 public class BubbleSort {
 
     void main() {
+        System.out.println(Arrays.stream(sort(new int[]{20, 9, 86, -2, 16})).mapToObj(x -> String.valueOf(x)).collect(joining(",")));
+        System.out.println(Arrays.stream(sort(new int[]{5, 4, 3, 2, 1})).mapToObj(x -> String.valueOf(x)).collect(joining(",")));
     }
 
-    public static int[] sort(int[] array) {
+    public static int[] sort(int[] nums) {
 
+        for (var i = 0; i < nums.length; i++) {
+            for (var j = 0; j < nums.length - 1; j++) {
+                if (nums[j] > nums[j + 1]) {
+                    var aux = nums[j + 1];
+                    nums[j + 1] = nums[j];
+                    nums[j] = aux;
+                }
+            }
+        }
+
+        return nums;
     }
 }
