bdb43652ee21128449d5b1bd7719cbe1cad2670e
Eduardo J <duduxss3@gmail.com>
2025-04-22 17:02:44 -0300

    :sparkles: (main) Adiciona algoritmo quicksort

---
+++ searches_and_sorts/exercises/sorts/QuickSort.java
@@ -0,0 +1,42 @@
+package src.searches_and_sorts.exercises.sorts;
+
+public class QuickSort {
+
+    void main() {
+        int[] nums = {5, 2, 9, 1, 5, 6};
+        quickSort(nums, 0, nums.length - 1);
+
+        for (int num : nums) {
+            System.out.print(STR."\{num} ");
+        }
+    }
+
+    public static void quickSort(int[] nums, int left, int right) {
+        if (left < right) {
+            int pivot = partition(nums, left, right);
+            quickSort(nums, left, pivot - 1);
+            quickSort(nums, pivot + 1, right);
+        }
+    }
+
+    public static int partition(int[] nums, int left, int right) {
+        int pivot = nums[right];
+        int i = left;
+
+        for (int j = left; j < right; j++) {
+            if (nums[j] <= pivot) {
+                swap(nums, j, i);
+                i++;
+            }
+        }
+
+        swap(nums, i, right);
+        return i;
+    }
+
+    public static void swap(int[] nums, int a, int b) {
+        int aux = nums[a];
+        nums[a] = nums[b];
+        nums[b] = aux;
+    }
+}
