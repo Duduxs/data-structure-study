5e60d142c458f3bdedb4017abf3f7058694e937d
Eduardo J <duduxss3@gmail.com>
2025-04-23 21:06:01 -0300

    :sparkles: (main) Adiciona LinkedList

--- src/main/java/org/edudev/lists/linkeds/LinkedList.java
+++ src/main/java/org/edudev/lists/linkeds/LinkedList.java
@@ -1,4 +1,27 @@
 package org.edudev.lists.linkeds;
 
-public class LinkedList {
+import org.edudev.lists.Node;
+
+public class LinkedList<T> {
+
+    private Node<T> head;
+    private int size;
+
+
+
+    public Node<T> getHead() {
+        return head;
+    }
+
+    public void setHead(Node<T> head) {
+        this.head = head;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
 }
