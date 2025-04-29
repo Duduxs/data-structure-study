d2f6fba62585e3f6492e3d38f2d50bb546c7f83d
Eduardo J <duduxss3@gmail.com>
2025-04-28 22:02:36 -0300

    :sparkles: (main) Adiciona 2 algoritmo de bubble sort

--- src/main/java/org/edudev/searches_and_sorts/exercises/sorts/BubbleSort.java
+++ src/main/java/org/edudev/searches_and_sorts/exercises/sorts/BubbleSort.java
@@ -9,6 +9,13 @@ public class BubbleSort {
         System.out.println(Arrays.stream(sort(new int[]{20, 9, 86, -2, 16})).mapToObj(x -> String.valueOf(x)).collect(joining(",")));
         System.out.println(Arrays.stream(sort(new int[]{5, 4, 3, 2, 1})).mapToObj(x -> String.valueOf(x)).collect(joining(",")));
         System.out.println(Arrays.stream(sort(new int[]{38, 23, 8, -5, 16, 29, 0, 13})).mapToObj(x -> String.valueOf(x)).collect(joining(",")));
+
+        System.out.println();
+        System.out.println();
+
+        System.out.println(Arrays.stream(sort2(new int[]{20, 9, 86, -2, 16})).mapToObj(x -> String.valueOf(x)).collect(joining(",")));
+        System.out.println(Arrays.stream(sort2(new int[]{5, 4, 3, 2, 1})).mapToObj(x -> String.valueOf(x)).collect(joining(",")));
+        System.out.println(Arrays.stream(sort2(new int[]{38, 23, 8, -5, 16, 29, 0, 13})).mapToObj(x -> String.valueOf(x)).collect(joining(",")));
     }
 
     /**
@@ -16,12 +23,12 @@ public class BubbleSort {
      * Ω(N) ⇾ Os elementos já estão ordenados.
      * Θ(N²) ⇾ Necessário ordenar parte dos elementos.
      * O(N²) ⇾ Os elementos estão na ordem inversa.
-     *
+     * <p>
      * Análise da complexidade de espaço
      * O(1) ⇾ A memória não aumenta conforme o tamanho da entrada.
      * Variáveis i, j, hasSwapped e aux só tem seus valores trocados por outros na stack durante o runtime,
      * mas sem adicionar memória.
-     *
+     * <p>
      * Não citei theta nem ômega, pois nesse caso eles tem o mesmo consumo de memória que o Big O.
      */
     public static int[] sort(int[] nums) {
@@ -43,6 +50,27 @@ public class BubbleSort {
             if (!hasSwapped) {
                 break;
             }
+        }
+
+        return nums;
+    }
+
+    public static int[] sort2(int[] nums) {
+
+        var hasSwapped = true;
+        var length = nums.length - 1;
+
+        while (hasSwapped) {
+            hasSwapped = false;
+            for (var i = 0; i < length; i++) {
+                if (nums[i] > nums[i + 1]) {
+                    var aux = nums[i + 1];
+                    nums[i + 1] = nums[i];
+                    nums[i] = aux;
+                    hasSwapped = true;
+                }
+            }
+            length--;
         }
 
         return nums;
