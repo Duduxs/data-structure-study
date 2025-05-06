ffd59dc293a615442ddd3f0aaed976b829dd2e03
Eduardo J <duduxss3@gmail.com>
2025-05-05 21:40:21 -0300

    :sparkles: (main) Novo método para adicionar elemento no final da lista duplamente encadeada

--- src/main/java/org/edudev/lists/linkeds/doubly/DoublyLinkedList.java
+++ src/main/java/org/edudev/lists/linkeds/doubly/DoublyLinkedList.java
@@ -6,6 +6,26 @@ public class DoublyLinkedList<T> {
     private NodeDoubly<T> tail;
     private int size;
 
+    public boolean isEmpty() {
+        return head == null;
+    }
+
+    public void addAtEnd(final T element) {
+        var node = new NodeDoubly(element);
+
+        if (isEmpty()) {
+            setHead(node);
+            setTail(node);
+            setSize(getSize() + 1);
+            return;
+        }
+
+        node.setPrev(getTail());
+        getTail().setNext(node);
+        setTail(node);
+        setSize(getSize() + 1);
+    }
+
     public NodeDoubly<T> getHead() {
         return head;
     }
--- src/main/java/org/edudev/lists/linkeds/doubly/Main.java
+++ src/main/java/org/edudev/lists/linkeds/doubly/Main.java
@@ -3,8 +3,17 @@ package org.edudev.lists.linkeds.doubly;
 public class Main {
 
     void main() {
-        var list = new DoublyLinkedList<NodeDoubly<String>>();
+        var list = new DoublyLinkedList<Integer>();
 
+        list.addAtEnd(20);
+        list.addAtEnd(9);
+        list.addAtEnd(86);
+        list.addAtEnd(-2);
+        list.addAtEnd(16);
+        list.addAtEnd(23);
+
+        System.out.println(list.getHead().getValue());
+        System.out.println(list.getTail().getValue());
 
     }
 }
