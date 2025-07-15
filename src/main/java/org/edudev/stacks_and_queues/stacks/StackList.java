e6c2c83db0e30574c920aebf55f2648d6cdfb3f9
Eduardo J <duduxss3@gmail.com>
2025-07-15 19:55:54 -0300

    :sparkles: (main) Adiciona implementação da Stack em lista e exercicio

---
+++ src/main/java/org/edudev/stacks_and_queues/stacks/StackList.java
@@ -0,0 +1,37 @@
+package org.edudev.stacks_and_queues.stacks;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class StackList<T> {
+
+    private List<T> stack = new ArrayList<>();
+
+    public void push(final T item) {
+        stack.add(item);
+    }
+
+    public T pop() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        final var element = stack.get(stack.size() - 1);
+        stack.remove(element);
+        return element;
+    }
+
+    public T peek() {
+        if (isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return stack.get(stack.size() - 1);
+    }
+
+    public int count() {
+        return stack.size();
+    }
+
+    public boolean isEmpty() {
+        return stack.isEmpty();
+    }
+}
--- src/main/java/org/edudev/stacks_and_queues/stacks/exercises/StackListExercise.java
+++ src/main/java/org/edudev/stacks_and_queues/stacks/exercises/StackListExercise.java
@@ -1,28 +1,23 @@
 package org.edudev.stacks_and_queues.stacks.exercises;
 
-import org.edudev.stacks_and_queues.stacks.StackArray;
+import org.edudev.stacks_and_queues.stacks.StackList;
 
-public class StackArrayExercise {
+public class StackListExercise {
 
     public void main() {
-        final var stack = new StackArray(4);
+        final var stack = new StackList<String>();
         System.out.println(STR."Pilha está vazia? \{stack.isEmpty()}");
         System.out.println(STR."Tamanho da pilha: \{stack.count()}");
-        System.out.println(STR."Pilha está cheia? \{stack.isFull()}");
         stack.push("Maria");
         stack.push("John");
         stack.push("Alice");
         System.out.println(STR."Pilha está vazia? \{stack.isEmpty()}");
-        System.out.println(STR."Tamanho da pilha: \{stack.count()}");
-        System.out.println(STR."Pilha está cheia? \{stack.isFull()}");
+        System.out.println(STR."Tamanho da pilha: \{stack.count()}");;
         System.out.println(STR."Topo da pilha: \{stack.peek()}");
 
         stack.push("Bob");
         System.out.println(STR."Tamanho da pilha: \{stack.count()}");
-        System.out.println(STR."Pilha está cheia? \{stack.isFull()}");
         System.out.println(STR."Topo da pilha: \{stack.peek()}");
-
-        //stack.push("Charlie ERROR");
 
         System.out.println();
         while (!stack.isEmpty()) {
@@ -34,4 +29,5 @@ public class StackArrayExercise {
         //stack.pop(); //Vai lançar um erro, pois a pilha está vazia
         //stack.peek(); //Vai lançar um erro, pois a pilha está vazia
     }
-}
+
+    }
