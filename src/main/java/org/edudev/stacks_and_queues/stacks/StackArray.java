8b0807a36b85396510dabb2b460fc97056e12017
Eduardo J <duduxss3@gmail.com>
2025-07-15 19:37:46 -0300

    :sparkles: (main) Adiciona restante das operações do StackArray e adiciona exercicio

--- src/main/java/org/edudev/stacks_and_queues/stacks/StackArray.java
+++ src/main/java/org/edudev/stacks_and_queues/stacks/StackArray.java
@@ -2,20 +2,42 @@ package org.edudev.stacks_and_queues.stacks;
 
 public final class StackArray {
 
-    private int size = 100;
+    private int size;
     private int top = -1;
-    private String[] stack = new String[size];
+    private String[] stack;
 
     public StackArray(final int size) {
         this.size = size;
+        this.stack = new String[size];
     }
 
     public void push(final String item) {
+        if(isFull()) {
+            throw new IllegalStateException("Stack is full");
+        }
         stack[++top] = item;
     }
 
     public String pop() {
+        if(isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
         return stack[top--];
+    }
+
+    public String peek() {
+        if(isEmpty()) {
+            throw new IllegalStateException("Stack is empty");
+        }
+        return stack[top];
+    }
+
+    public int count() {
+        return top + 1;
+    }
+
+    public boolean isFull() {
+        return top == size - 1;
     }
 
     public boolean isEmpty() {
--- src/main/java/org/edudev/stacks_and_queues/stacks/exercises/StackArrayExercise.java
+++ src/main/java/org/edudev/stacks_and_queues/stacks/exercises/StackArrayExercise.java
@@ -5,17 +5,33 @@ import org.edudev.stacks_and_queues.stacks.StackArray;
 public class StackArrayExercise {
 
     public void main() {
-        final var stack = new StackArray(10);
-        System.out.println(STR."Lista está vazia? \{stack.isEmpty()}");
+        final var stack = new StackArray(4);
+        System.out.println(STR."Pilha está vazia? \{stack.isEmpty()}");
+        System.out.println(STR."Tamanho da pilha: \{stack.count()}");
+        System.out.println(STR."Pilha está cheia? \{stack.isFull()}");
         stack.push("Maria");
         stack.push("John");
         stack.push("Alice");
-        System.out.println(STR."Lista está vazia? \{stack.isEmpty()}");
+        System.out.println(STR."Pilha está vazia? \{stack.isEmpty()}");
+        System.out.println(STR."Tamanho da pilha: \{stack.count()}");
+        System.out.println(STR."Pilha está cheia? \{stack.isFull()}");
+        System.out.println(STR."Topo da pilha: \{stack.peek()}");
 
+        stack.push("Bob");
+        System.out.println(STR."Tamanho da pilha: \{stack.count()}");
+        System.out.println(STR."Pilha está cheia? \{stack.isFull()}");
+        System.out.println(STR."Topo da pilha: \{stack.peek()}");
+
+        //stack.push("Charlie ERROR");
+
+        System.out.println();
         while (!stack.isEmpty()) {
             // LIFO (Last in First Out) O primeiro a entrar é o último a sair
             // (1) Alice -> (2) John -> (3) Maria
             System.out.println(stack.pop());
         }
+
+        //stack.pop(); //Vai lançar um erro, pois a pilha está vazia
+        //stack.peek(); //Vai lançar um erro, pois a pilha está vazia
     }
 }
