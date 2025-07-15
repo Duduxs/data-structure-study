4d9d58d7c65af6e9d3171dd3d3e1cf1599c39096
Eduardo J <duduxss3@gmail.com>
2025-07-15 20:58:34 -0300

    :sparkles: (main) Adiciona mais uma solução para o problema isBalanced

--- src/main/java/org/edudev/stacks_and_queues/stacks/exercises/IsBalanced.java
+++ src/main/java/org/edudev/stacks_and_queues/stacks/exercises/IsBalanced.java
@@ -8,6 +8,9 @@ public final class IsBalanced {
     void main() {
         System.out.println(isBalanced("(())()"));
         System.out.println(isBalanced("())("));
+        System.out.println();
+        System.out.println(isBalanced2("(())()"));
+        System.out.println(isBalanced2("())("));
     }
 
     public static boolean isBalanced(final String value) {
@@ -23,6 +26,22 @@ public final class IsBalanced {
             if (parentheses.pollFirst() == null) return false;
         }
 
-        return true;
+        return parentheses.isEmpty();
+    }
+
+    public static boolean isBalanced2(final String value) {
+        final Deque<Character> parentheses = new ArrayDeque<>();
+
+        for (var i = 0; i < value.length(); i++) {
+
+            if (value.charAt(i) == '(') {
+                parentheses.push(value.charAt(i));
+            } else if (value.charAt(i) == ')') {
+                if (parentheses.isEmpty()) return false;
+                parentheses.pop();
+            }
+        }
+
+        return parentheses.isEmpty();
     }
 }
