337cab0618217c83641ed6af91ea1a4a66092489
Eduardo J <duduxss3@gmail.com>
2025-07-15 21:51:50 -0300

    :sparkles: (main) Adiciona 1 solução pro metodo RemoveDuplicates

---
+++ src/main/java/org/edudev/stacks_and_queues/stacks/exercises/RemoveDuplicates.java
@@ -0,0 +1,31 @@
+package org.edudev.stacks_and_queues.stacks.exercises;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public final class RemoveDuplicates {
+
+    public void main() {
+        System.out.println(removeDuplicates("abbaca")); //ca
+        System.out.println(removeDuplicates("azxxzy"));//ay
+    }
+
+    public String removeDuplicates(final String s) {
+        Deque<Character> stack = new ArrayDeque<>();
+        final var sb = new StringBuilder();
+        for (var i = 0; i < s.length(); i++) {
+            final var element = s.charAt(i);
+            if (stack.isEmpty() || stack.peek() != element) {
+                stack.push(element);
+            } else {
+                stack.pop();
+            }
+        }
+
+        while(!stack.isEmpty()) {
+            sb.append(stack.pollLast());
+        }
+
+        return sb.toString();
+    }
+}
