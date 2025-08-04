b468884b9e0eb1ada1eb259f29ac3e643b34a0ac
Eduardo J <duduxss3@gmail.com>
2025-08-04 18:21:31 -0300

    :sparkles: (main) Adiciona 2 solução IntersectionOfTwoArrays

--- src/main/java/org/edudev/sets/exercises/IntersectionOfTwoArrays.java
+++ src/main/java/org/edudev/sets/exercises/IntersectionOfTwoArrays.java
@@ -7,18 +7,24 @@ import java.util.Set;
 final class IntersectionOfTwoArrays {
 
     void main() {
-        Arrays.stream(intersection(new int[]{1,2,2,1}, new int[]{2,2})).forEach(System.out::println);
+        Arrays.stream(intersection(new int[]{1, 2, 2, 1}, new int[]{2, 2})).forEach(System.out::println);
         System.out.println("-----------------");
-        Arrays.stream(intersection(new int[]{4,9,5}, new int[]{9,4,9,8,4})).forEach(System.out::println);
+        Arrays.stream(intersection(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4})).forEach(System.out::println);
         System.out.println("-----------------");
+        System.out.println();
+        Arrays.stream(intersectionTwo(new int[]{1, 2, 2, 1}, new int[]{2, 2})).forEach(System.out::println);
+        System.out.println("-----------------");
+        Arrays.stream(intersectionTwo(new int[]{4, 9, 5}, new int[]{9, 4, 9, 8, 4})).forEach(System.out::println);
+        System.out.println("-----------------");
+
     }
 
     public static int[] intersection(final int[] nums1, final int[] nums2) {
         final Set<Integer> setNums1 = new HashSet<>();
         final Set<Integer> setNums2 = new HashSet<>();
 
-        for(var i = 0; i < nums1.length ; i++) setNums1.add(nums1[i]);
-        for(var i = 0; i < nums2.length ; i++) setNums2.add(nums2[i]);
+        for (var i = 0; i < nums1.length; i++) setNums1.add(nums1[i]);
+        for (var i = 0; i < nums2.length; i++) setNums2.add(nums2[i]);
 
         setNums1.retainAll(setNums2);
 
@@ -27,9 +33,30 @@ final class IntersectionOfTwoArrays {
         final int[] result = new int[setNums1.size()];
         int i = 0;
 
-        for(final var num: setNums1) result[i++] = num;
+        for (final var num : setNums1) result[i++] = num;
 
         return result;
     }
+
+    public static int[] intersectionTwo(final int[] nums1, final int[] nums2) {
+        final Set<Integer> setNums1 = new HashSet<>();
+        final Set<Integer> intersection = new HashSet<>();
+
+        for (var i = 0; i < nums1.length; i++)
+            setNums1.add(nums1[i]);
+
+        for (var i = 0; i < nums2.length; i++)
+            if (setNums1.contains(nums2[i]))
+                intersection.add(nums2[i]);
+
+        final int[] result = new int[intersection.size()];
+        int i = 0;
+
+        for (final var num : intersection)
+            result[i++] = num;
+
+        return result;
+    }
+
 
 }
