a61d27bc41b04d2c40639ae2763d919999a5f45c
Eduardo J <duduxss3@gmail.com>
2025-08-26 19:45:39 -0300

    :sparkles: (main) Adiciona 1 solução TwoSum

---
+++ src/main/java/org/edudev/maps/exercises/TwoSum.java
@@ -0,0 +1,37 @@
+package org.edudev.maps.exercises;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+    void main() {
+        Arrays.stream(twoSum(new int[]{8, 2, 7, 15}, 9)).forEach(System.out::println); // saída 1,2
+        System.out.println();
+        Arrays.stream(twoSum(new int[]{3, 2, 4}, 6)).forEach(System.out::println); // saída 1,2
+        System.out.println();
+        Arrays.stream(twoSum(new int[]{3, 5, 7, 2, 4, 8, 1, 6}, 15)).forEach(System.out::println); // saída 2,5
+        System.out.println();
+    }
+
+    public static int[] twoSum(final int[] nums, final int target) {
+        final Map<Integer, Integer> map = new HashMap<>();
+        final var result = new int[2];
+
+        for (var i = 0; i < nums.length; i++) {
+            final var difference = target - nums[i];
+            final var mapValue = map.get(difference);
+
+            if (mapValue != null) {
+                result[0] = i;
+                result[1] = mapValue;
+                break;
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return result;
+    }
+}
